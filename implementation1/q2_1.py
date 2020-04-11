c26ca1ffb533b72279bb4a0d2878cd63a8355f24
muchgangster <jbarringer1999@gmail.com>
2020-04-11 13:39:11 -0700

    added plotting and ASE testing

--- implementation1/q2_1.py
+++ implementation1/q2_1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import argparse
+import matplotlib.pyplot as plt
 
 formatter = "{:.2f}".format
 np.set_printoptions(formatter={'float_kind':formatter})
@@ -7,10 +8,37 @@ np.set_printoptions(formatter={'float_kind':formatter})
 def sigmoid(W, X):
     return 1 / (1 + np.exp(np.negative(W.transpose().dot(X))))
 
-def train(X, Y, e, lr):
+# run_model calculates the ASE
+def run_model(X, Y, W):
+    # Place holder matrix that will hold the X rows times the weights
+    wX = np.zeros(X.shape)
+
+    # For every row in X, multiply elementwise by W
+    for i in range(wX.shape[0]):
+        row = np.multiply(X[i], W)
+        wX[i] = row
+
+        # Get the predicted Y by summing the row
+        predicted_y = wX.sum(axis=1)
+
+    sumY = 0
+    # Sum the squared error between Y and predicted Y
+    for y, yH in zip(Y,predicted_y):
+        sumY += (y - yH)**2 
+
+    # Average for the average squared error
+    ase = sumY / Y.size
+    return ase
+
+def train(X, Y, tX, tY, e, lr):
     W = np.zeros(X.shape[1])
     
+    i = 0
     while True:
+        # track number of iterations
+        i += 1
+        iterations.append(i)
+
         delta = np.zeros(X.shape[1])
 
         for i in range(X.shape[0]):
@@ -18,6 +46,9 @@ def train(X, Y, e, lr):
             delta = delta + (y_hat - Y[i]) * X[i]
 
         W = W - lr * delta
+
+        rASE.append(run_model(X, Y, W))
+        tASE.append(run_model(tX, tY, W))
 
         if np.linalg.norm(delta) <= e:
             break
@@ -39,6 +70,19 @@ def get_data(filename):
     return X, Y
 
 X, Y = get_data('usps_train.csv')
+tX, tY = get_data('usps_test.csv')
 
-W = train(X, Y, 10, 0.0000005)
-print(W)
+iterations = []
+rASE = []
+tASE = []
+
+W = train(X, Y, tX, tY, 10, 0.0000005)
+
+# plot data with matplotlib
+training_plot = plt.plot(iterations, rASE, 'bo--', label="Training ASE")
+testing_plot = plt.plot(iterations, tASE, 'gs--', label="Testing ASE")
+plt.xlabel('Number of Iterations')
+plt.ylabel('ASE Value')
+plt.legend()
+plt.title("Training and Testing ASE vs. Iteration Count")
+plt.show()

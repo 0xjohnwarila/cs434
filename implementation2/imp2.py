7711c91b91601dad059510b7c18223622e1f1b87
JohnNWarila <johnwarila@gmail.com>
2020-04-26 08:16:12 -0700

    Added run_type and docs

--- implementation2/imp2.py
+++ implementation2/imp2.py
@@ -31,6 +31,7 @@ def clean_labels(labels):
     """clean_labels return a list with positive label as 1 and negative as 0"""
     ret = np.zeros(len(labels))
     count = 0
+    # Encodes positive as 1, and negative as 0
     for label in labels:
         if label == 'positive':
             ret[count] = 1
@@ -60,52 +61,61 @@ def training(data, labels, vocab, alpha, vec):
             document_set[1].append(data[i])
 
     # Laplace smoothing
-    positive = vec.transform(document_set[1])
-    negative = vec.transform(document_set[0])
+    positive = vec.transform(document_set[1]).toarray()
+    negative = vec.transform(document_set[0]).toarray()
 
-    positive = positive.toarray()
-    negative = negative.toarray()
-    positive_1 = np.sum(positive, axis=0)
-    negative_1 = np.sum(negative, axis=0)
+    # Find the total word count for each corpus
+    positive_count = np.sum(positive, axis=0)
+    negative_count = np.sum(negative, axis=0)
 
-    positive = positive_1 + alpha
-    negative = negative_1 + alpha
+    # Find the numerator for the calculation, adding alpha for laplace
+    # smoothing
+    positive = positive_count + alpha
+    negative = negative_count + alpha
 
-    ls_pos = np.sum(positive_1) + (len(vocab)*alpha)
-    ls_neg = np.sum(negative_1) + (len(vocab)*alpha)
+    # Find the denominator, the sum of all words in each corpus plus the number
+    # of words in the vocabulary times alpha
+    smoothing_denom_pos = np.sum(positive_count) + (len(vocab)*alpha)
+    smoothing_denom_neg = np.sum(negative_count) + (len(vocab)*alpha)
 
-    positive = np.true_divide(positive, ls_pos)
-    negative = np.true_divide(negative, ls_neg)
+    # Divide numerator by denominator, this is now the probability for each
+    # word given the class
+    positive = np.true_divide(positive, smoothing_denom_pos)
+    negative = np.true_divide(negative, smoothing_denom_neg)
 
-    return priors, positive, negative
+    return priors, (negative, positive)
 
 
 def predict(probs, doc):
     """Predict returns the naive bayes prediction for a document"""
     ret = 1
-    doc = doc.flatten()
 
+    # For every word in the document, find the product of the probabilities,
+    # raised by the frequency of the word
     for prob, word in zip(probs, doc):
         ret *= prob ** word
 
+    # Return the total probability of the document's words being in the class
     return ret
 
 
-def validate(positive, negative, priors, data, labels, run):
+def validate(probs, priors, data, labels, tag):
     """validate tests the entire set of data and labels, and prints the percent
     correct"""
+
+    # Format document for use
     doc = data[0].toarray()
     count = 0
 
     for doc, label in zip(data, labels):
-        pos = priors[1] * predict(positive, doc.toarray())
-        neg = priors[0] * predict(negative, doc.toarray())
+        pos = priors[1] * predict(probs[1], doc.toarray().flatten())
+        neg = priors[0] * predict(probs[0], doc.toarray().flatten())
 
         if pos > neg and label == 1:
             count += 1
         elif pos < neg and label == 0:
             count += 1
-    print(run, 100 * (count / len(labels)))
+    print(tag, 100 * (count / len(labels)))
 
 
 def run(data, labels, alpha, max_features, max_df, min_df):
@@ -126,74 +136,158 @@ def run(data, labels, alpha, max_features, max_df, min_df):
     vectorizer.fit(imdb_data['review'])
 
     # get the vocabulary
-    inv_vocab = {v: k for k, v in vectorizer.vocabulary_.items()}
-    vocabulary = [inv_vocab[i] for i in range(len(inv_vocab))]
+    vocabulary = {v: k for k, v in vectorizer.vocabulary_.items()}
+    vocabulary = [vocabulary[i] for i in range(len(vocabulary))]
 
-    # Get label data and clean it for use
-
-    class_data = pd.read_csv(labels, delimiter=',')
-    labels = class_data['sentiment']
-
-    labels = clean_labels(labels)
     training_data = imdb_data['review'][:30000]
     validation_data = imdb_data['review'][30000:40000]
 
-    training_labels = labels[:30000]
-    validation_labels = labels[30000:]
+    # Get label data and clean it for use
 
-    priors, positive, negative = training(
+    labels = clean_labels(pd.read_csv(labels, delimiter=',')['sentiment'])
+
+    training_labels = labels[:30000]
+    validation_labels = labels[30000:40000]
+
+    # Run the training algorithm
+    priors, probs = training(
         training_data,
         training_labels,
         vocabulary,
         alpha,
         vectorizer
     )
+
+    # Validation run
     validate(
-        positive, negative, priors,
+        probs,
+        priors,
         vectorizer.transform(validation_data),
         validation_labels,
         'validation'
     )
+
+    # Training run
     validate(
-        positive, negative, priors,
+        probs,
+        priors,
         vectorizer.transform(training_data),
         training_labels,
         'training'
     )
 
+
+def run_test(data, labels, alpha, max_features, max_df, min_df):
+    """run_test runs the algorithm on the testing data"""
+    imdb_data = pd.read_csv(data, delimiter=',')
+
+    vectorizer = CountVectorizer(
+        stop_words='english',
+        preprocessor=clean_text,
+        max_features=max_features,
+        max_df=max_df,
+        min_df=min_df
+    )
+
+    # get training/testing data and vocabulary
+    vectorizer.fit(imdb_data['review'])
+    vocabulary = {v: k for k, v in vectorizer.vocabulary_.items()}
+    vocabulary = [vocabulary[i] for i in range(len(vocabulary))]
+
+    training_data = imdb_data['review'][:30000]
+    testing_data = imdb_data['review'][40000:]
+
+    # get labels
+    labels = clean_labels(pd.read_csv(labels, delimiter=',')['sentiment'])
+    training_labels = labels[:30000]
+    testing_labels = labels[40000:]
+
+    # Train
+    priors, probs = training(
+        training_data,
+        training_labels,
+        vocabulary,
+        alpha,
+        vectorizer
+    )
+
+    # Run testing data
+    validate(
+        probs,
+        priors,
+        vectorizer.transform(testing_data),
+        testing_labels,
+        'testing'
+    )
+
+
+# Arguments parsing
+
 PARSER = argparse.ArgumentParser(description='Multinomial Naive Bayes')
 PARSER.add_argument('data', help='csv file with reviews')
 PARSER.add_argument('labels', help='csv file with labels for data')
-PARSER.add_argument('alpha', type=float, help='alpha value for laplace smoothing')
-PARSER.add_argument('max_features', type=int, help='Maximum features for BOW')
-PARSER.add_argument('max_df', type=float, help='max_df for BOW (float form)')
-PARSER.add_argument('min_df', type=float, help='min_df for BOW (float form)')
+PARSER.add_argument('run_type', help='See README for run_types')
+PARSER.add_argument('--alpha',
+                    default=1,
+                    type=float,
+                    help='alpha value/laplace smoothing')
+PARSER.add_argument('--max_features',
+                    default=2000,
+                    type=int,
+                    help='Maximum features for BOW')
+PARSER.add_argument('--max_df',
+                    default=1,
+                    type=float,
+                    help='max_df for BOW (float form)')
+PARSER.add_argument('--min_df', default=1,
+                    type=float,
+                    help='min_df for BOW (float form)')
 
 # BEST parameters right now: Alpha = 1, max_features=368, max_df=0.34,
 # min_df=0.037
-args = PARSER.parse_args()
-run(args.data, args.labels, args.alpha, args.max_features, args.max_df,
-        args.min_df)
+ARGS = PARSER.parse_args()
 
+# Switch on the run_type
+if ARGS.run_type == "validate_default":
+    run(ARGS.data, ARGS.labels, 1, 2000, 1.0, 1)
+elif ARGS.run_type == "validate_best":
+    run(ARGS.data, ARGS.labels, 1, 368, 0.34, 0.037)
+elif ARGS.run_type == "test_default":
+    print("Make sure all 50k labels are in the labels file")
+    run_test(ARGS.data, ARGS.labels, 1, 2000, 1.0, 1)
+elif ARGS.run_type == "test_best":
+    print("Make sure all 50k labels are in the labels file")
+    run_test(ARGS.data, ARGS.labels, 1, 2000, 0.34, 0.037)
+elif ARGS.run_type == "cust":
+    print("Custom run, training and validation only")
+    run(ARGS.data, ARGS.labels, ARGS.alpha, ARGS.max_features, ARGS.max_df,
+        ARGS.min_df)
+else:
+    print("Unknown runtype, please use on of these:")
+    print("validate_default, validate_best, test_default, test_best")
+
+
+# Below this line is leftover tuning scripts. Left for posterity
+
+"""
 max_dfs = np.arange(.3, 0.4, 0.01)
 min_dfs = np.arange(0.02, 0.05, 0.001)
 alphas = np.arange(0, 5, 0.2)
+"""
 
 """
 for alpha in alphas:
     print("----- RUNNING WITH alpha =", alpha, "-----") 
     print("-")
-    run(args.data, args.labels, alpha, args.max_features, args.max_df, 
-        args.min_df)
+    run(ARGS.data, ARGS.labels, alpha, ARGS.max_features, ARGS.max_df, 
+        ARGS.min_df)
     print("-")
-"""
-
-"""
+""" """
 for max_df in max_dfs:
     print("----- RUNNING WITH max_df =", max_df, "-----")
     print("-")
-    run(args.data, args.labels, args.alpha, args.max_features, max_df,
-            args.min_df)
+    run(ARGS.data, ARGS.labels, ARGS.alpha, ARGS.max_features, max_df,
+            ARGS.min_df)
     print("-")
 """
 
@@ -201,7 +295,7 @@ for max_df in max_dfs:
 for min_df in min_dfs:
     print("----- RUNNING WITH min_df =", min_df, "-----")
     print("-")
-    run(args.data, args.labels, args.alpha, args.max_features, args.max_df,
+    run(ARGS.data, ARGS.labels, ARGS.alpha, ARGS.max_features, ARGS.max_df,
             min_df)
     print("-")
 """
@@ -212,8 +306,8 @@ max_features_list = np.arange(366, 376, 1)
 for max_features in max_features_list:
     print("----- RUNNING WITH max_features =", max_features, "-----")
     print("-")
-    run(args.data, args.labels, args.alpha, max_features, args.max_df,
-            args.min_df)
+    run(ARGS.data, ARGS.labels, ARGS.alpha, max_features, ARGS.max_df,
+            ARGS.min_df)
     print("-")
 """
 
